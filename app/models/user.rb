fa46c275ca8193c2142d664fd83b580329df012e
Oscar Fabra <oscarfabra@gmail.com>
2014-12-22 15:00:41 -0500

    Small fix on user auth form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
+
+  # attr_accessor :signin
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
 end
