f71a132bd3885048a20009eb81805565d1ff82b5
Oscar Fabra <oscarfabra@gmail.com>
2014-12-22 16:33:18 -0500

    Overrode Devise's registraations_controller; User can edit its data.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'registrations/update'
+
   get 'home/index'
 
-  devise_for :users
+  # Tells Rails to use registrations_controller instead of the default.
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
