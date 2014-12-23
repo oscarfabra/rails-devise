eeb45e32a45d44d719881859f691b5ca7f6eb8b3
Oscar Fabra <oscarfabra@gmail.com>
2014-12-22 20:28:05 -0500

    New user sign up with confirmation.

---
+++ db/migrate/20141223011856_add_confirmable_to_users.rb
@@ -0,0 +1,20 @@
+class AddConfirmableToUsers < ActiveRecord::Migration
+  def up
+    add_column :users, :unconfirmed_email, :string
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :string
+    add_column :users, :confirmation_sent_at, :datetime
+    add_index :users, :confirmation_token, :unique => true
+
+    # Current data will be treated as if they have confirmed their account.
+    User.update_all(:confirmed_at => Time.now)
+  end
+
+  def down
+    remove_column :users,
+                  :uncofirmed_email,
+                  :confirmation_token,
+                  :confirmed_at,
+                  :confirmation_sent_at
+  end
+end
