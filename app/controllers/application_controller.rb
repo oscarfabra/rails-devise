6b5b5906f5f8259a2f7844e4a3e61e193121b3b6
Oscar Fabra <oscarfabra@gmail.com>
2014-12-23 17:10:58 -0500

    Added custom layouts for devise models.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,25 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  # Sets a different layout for each devise resource.
+  layout :layout_by_resource
+
   protected
+
+    # Sets a different layout for each devise resource.
+    def layout_by_resource
+      if devise_controller?
+        if resource_name == :admin
+          "devise_admin_application" # admin model will use this layout.
+        elsif resource_name == :employee
+          "devise_employee_application" # employee model will use this layout.
+        else
+          "devise_application" # any other devise model will use this layout.
+        end
+      else
+        "application" # default rails application layout.
+      end
+    end
 
     # Configures mass assignment rules for the devise controllers
     def configure_permitted_parameters
